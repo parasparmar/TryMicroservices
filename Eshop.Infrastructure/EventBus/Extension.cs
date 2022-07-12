197193415ba8e4cac10841f4a8aef7c35c739909
Paras Parmar <parasparmar@gmail.com>
2022-07-13 00:29:14 +0530

    Added Rabbit MQ to the Infrastructure

---
+++ Eshop.Infrastructure/EventBus/Extension.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Eshop.Infrastructure.EventBus
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddRabbitMq(this IServiceCollection services, IConfiguration configuration)
+        {
+            var rabbitMq = new RabbitMQOption();
+            configuration.GetSection("rabbitmq").Bind(rabbitMq);
+            // establish connection with rabbitMQ..
+            services.AddMassTransit(x => {
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                {
+                    cfg.Host(new Uri(rabbitMq.ConnectionString), hostcfg => {
+                        hostcfg.Username(rabbitMq.Username);
+                        hostcfg.Password(rabbitMq.Password);
+                    });
+                    cfg.ConfigureEndpoints(provider);
+                }));                
+            });
+
+            //TODO: Locate this deprecated service dependency
+            //services.AddMassTransitHostedService();
+            return services;
+
+        }
+    }
+
+}
---
+++ Eshop.Infrastructure/EventBus/RabbitMQOption.cs
@@ -0,0 +1,11 @@
+﻿namespace Eshop.Infrastructure.EventBus
+{
+    public class RabbitMQOption
+    {
+
+        public string ConnectionString { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
